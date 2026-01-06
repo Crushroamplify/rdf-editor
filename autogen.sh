4bbdd71a6bacb505ce18da8eb4841e41d85dc489
Patrick van Kleef <pkleef@openlinksw.com>
2026-01-06 11:44:58 +0000

    Updated Copyright

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@
 #
 #  This file is part of the OpenLink RDF Editor
 #
-#  Copyright (C) 2014-2025 OpenLink Software
+#  Copyright (C) 2014-2026 OpenLink Software
 #
 #  This project is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the
